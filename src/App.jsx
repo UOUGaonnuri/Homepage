0871a56d4c9761baceac3bb874eb92e668192c00
현영 <hyun0@mail.ulsan.ac.kr>
2026-02-21 15:15:30 +0900

    chore: 레이아웃 및 페이지 라우팅 초기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Layout from './layout/Layout'
+import HomePage from './pages/HomePage'
+import ActivitiesPage from './pages/ActivitiesPage'
+import ProjectsPage from './pages/ProjectsPage'
+import RecruitmentPage from './pages/RecruitmentPage'
+import QnAPage from './pages/QnAPage'
+import NotFoundPage from './pages/NotFoundPage'
 
-const App = () => {
-  return <div className="bg-red-50">App</div>;
-};
 
-export default App;
+const routes = [
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'activities', element: <ActivitiesPage /> },
+      { path: 'projects', element: <ProjectsPage /> },
+      { path: 'recruitment', element: <RecruitmentPage /> },
+      { path: 'qna', element: <QnAPage /> },
+    ],
+  },
+]
+
+const router = createBrowserRouter(routes)
+
+function App() {
+  return (
+      <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/pages/RecruitmentPage.jsx
@@ -0,0 +1,5 @@
+export default function RecruitmentPage() {
+  return (
+    <div>RecruitmentPage</div>
+  )
+}
